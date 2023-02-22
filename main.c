9f1b212348018c011ed202f6fa22d6e9523efc9f
lukaszrychlowski <rychlowski@agh.edu.pl>
2023-02-23 00:24:13 +0100

    reduce complexity of interrupt, big numbers

--- main.c
+++ main.c
@@ -17,6 +17,7 @@
 
 /// variables
 volatile uint8_t count = 0;
+volatile uint8_t change_val;
 volatile uint8_t output1_count = 0;
 volatile uint8_t output2_count = 0;
 volatile uint8_t output3_count = 0;
@@ -49,11 +50,10 @@ void set_output3(volatile uint8_t i){
 }
 
 /// encoder change value of the variable, up or down
-int change_val(bool Up){
-    count = Up ? 1:-1;
-    return count;
+int change_value(bool Up){
+    change_val = Up ? 1:-1;
+    return change_val;
 }
-volatile int dum = 0;
 
 int main(void){
     oled_init();
@@ -67,25 +67,29 @@ int main(void){
     SREG = (1 << 7);                                    // global interrupt enable
     PCMSK0 = (1 << encoder_A) | (1 << encoder_button);  // set interrupt event to trigger on state-change of encoder pins
     PCICR = (1 << PCIE0);                               // pin change interrupt enabled
-    EICRA = (1 << ISC01);                               // falling edge of int0 generates interrupt req
+    EICRA = (1 << ISC01) | (1 << ISC00);                 // rising edge of int0 generates interrupt req
     PCIFR = (1 << PCIF0);                               // clear interrupt flag
     //sei();
     while(1){
-        // dum += 1;
-        // oled_print_temp(dum,0,15);
-        // _delay_ms(50);
+        set_output1(output1_count);
+        oled_print_output1(output1_count);
+        set_output2(output2_count);
+        oled_print_output2(output2_count);
+        set_output3(output3_count);
+        oled_print_output3(output3_count);
     }
 }
 
 ISR(PCINT0_vect){
-    uint8_t a = PINB >> encoder_A & 1;                  // read pins state
+    
+    uint8_t a = PINB >> encoder_A & 1;                  // read pins state, sets a to 0 or 1
     uint8_t b = PINB >> encoder_B & 1;
     uint8_t s = PINB >> encoder_button & 1;
     if (a != a0){                                       // check if encoder was rotated
         a0 = a;
         if (b != c0){                                   // clockwise, or counterclockwise?
             c0 = b;
-            count = count + change_val(a == b);
+            count = count + change_value(a == b);
         }
     }
     else if(s != s0){                                   // check if button was pressed
@@ -95,29 +99,28 @@ ISR(PCINT0_vect){
             output_event += 1;
             button_event = 0;
             if(output_event > 3) output_event = 1;
-            dum += 100;
         }
     }
-    
+        
     if(output_event == 1){
         output1_count = output1_count + count;
-        oled_print_output1(output1_count);
-        set_output1(output1_count);
+        
+        //set_output1(output1_count);
         count = 0;
     }
-        else if(output_event == 2){
-            output2_count = output2_count + count;
-            oled_print_output2(output2_count);
-            set_output2(output2_count);
-            count = 0;
-        }
-        else if(output_event == 3){
-            output3_count = output3_count + count;
-            oled_print_output3(output3_count);
-            set_output3(output3_count);
-            count = 0;
-        }
-    
-  }
+    else if(output_event == 2){
+        output2_count = output2_count + count;
+        
+        //set_output2(output2_count);
+        count = 0;
+    }
+    else if(output_event == 3){
+        output3_count = output3_count + count;
+        
+        //set_output3(output3_count);
+        count = 0;
+    }
+}
+  
 
 
